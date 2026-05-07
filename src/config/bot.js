69a7c4cadcb9e5a58f9b0238b91ebe67fb5fa602
kalefler197-dotcom <kalefler197@gmail.com>
2026-05-07 09:26:02 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Tech is a GOOY BOY ❤️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
